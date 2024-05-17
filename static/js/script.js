fccfed98bd51cffcf20ab91a87e228b54b102652
charly1joseph <charly1joseph@outlook.com>
2024-05-17 13:54:12 +0000

    smooth button2

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
+            if (button.classList.contains('selected')) {
+                button.classList.remove('selected');
+                button.querySelector('.button-text').classList.add('hidden');
+                button.style.width = '50px';
+                return;
+            }
+
             buttons.forEach(btn => {
                 btn.classList.remove('selected');
                 btn.querySelector('.button-text').classList.add('hidden');
@@ -12,19 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
             button.classList.add('selected');
             button.querySelector('.button-text').classList.remove('hidden');
 
-            const buttonIndex = Array.from(buttons).indexOf(button);
-            const totalButtons = buttons.length;
-
             const extraWidth = button.querySelector('.button-text').scrollWidth + 16; // 16 for padding
             button.style.width = `${50 + extraWidth}px`;
-
-            buttons.forEach((btn, index) => {
-                if (index < buttonIndex) {
-                    btn.style.marginRight = '0';
-                } else if (index > buttonIndex) {
-                    btn.style.marginLeft = '0';
-                }
-            });
         });
     });
 });
